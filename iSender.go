dc157d5c82498ef6359c833a8e22bdb662c38591
Rangding Zhang <love-nankai@163.com>
2018-05-05 10:18:46 +0800

    Update iSender.go

--- iSender.go
+++ iSender.go
@@ -1,5 +1,5 @@
 package main
- 
+
 import (
 	"sync/atomic"
 	"fmt"
@@ -10,64 +10,63 @@ import (
 	"encoding/json"
 	"iSender/util"
 )
- 
+
 var succeeded uint64
-var sendedRequests uint64
- 
+var sended uint64
+
 func main() {
 	fmt.Println("Begin-- threads:", util.Input.Threads, ", requests for each thread:", util.Input.Requests)
-	for i:=uint64(0); i<util.Input.Threads; i++ {
+	for i := uint64(0); i < util.Input.Threads; i++ {
 		go request()
 	}
-	for atomic.LoadUint64(&sendedRequests) < util.Input.Threads * util.Input.Requests {
+	for atomic.LoadUint64(&sended) < util.Input.Threads*util.Input.Requests {
 		time.Sleep(time.Second)
 	}
-	fmt.Println("End-- sended requests:", atomic.LoadUint64(&sendedRequests), ", succeed:", atomic.LoadUint64(&succeeded))
+	fmt.Println("End-- sended requests:", atomic.LoadUint64(&sended), ", succeed:", atomic.LoadUint64(&succeeded))
 }
- 
+
 func request() {
-	for i:=uint64(0); i < util.Input.Requests; i++ {
-		var client *util.Client
-		var err error
-		if util.Input.Ca == "" {
-			client = util.NewClient()
-		} else if util.Input.Cert == "" {
-			client, err = util.NewClientWithCaPath(util.Input.Ca)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		} else {
-			client, err = util.NewClientWithCertFiles(util.Input.Ca, util.Input.Cert, util.Input.Key)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
-		body := bytes.NewReader([]byte(util.Input.Body))
-		var headers map[string]string
-		if util.Input.Headers != "" {
-			err = json.Unmarshal([]byte(util.Input.Headers), &headers)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
- 
-		request, err := util.NewRequest(util.Input.Method, util.Input.Url, body, headers)
+	var client *util.Client
+	var err error
+	if util.Input.Ca == "" {
+		client = util.NewClient()
+	} else if util.Input.Cert == "" {
+		client, err = util.NewClientWithCaFile(util.Input.Ca)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
+	} else {
+		client, err = util.NewClientWithCertFiles(util.Input.Ca, util.Input.Cert, util.Input.Key)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	body := bytes.NewReader([]byte(util.Input.Body))
+	var headers map[string]string
+	if util.Input.Headers != "" {
+		err = json.Unmarshal([]byte(util.Input.Headers), &headers)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	request, err := util.NewRequest(util.Input.Method, util.Input.Url, body, headers)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for i := uint64(0); i < util.Input.Requests; i++ {
 		response, err := client.SendRequest(request)
 		if err == nil {
 			resBody, _ := ioutil.ReadAll(response.Body)
-			fmt.Println("response CODE:", response.StatusCode, "; response BODY:",string(resBody))
+			fmt.Println("response CODE:", response.StatusCode, "; response BODY:", string(resBody))
 			atomic.AddUint64(&succeeded, 1)
 		} else {
-			fmt.Println("err:", err)
+			fmt.Println("error:", err)
 		}
-		atomic.AddUint64(&sendedRequests, 1)
+		atomic.AddUint64(&sended, 1)
 		time.Sleep(time.Duration(util.Input.Delay) * time.Millisecond)
 	}
 }
