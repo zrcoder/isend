84d921600ac2874c594983b204e1bf28ea764675
Rangding Zhang <love-nankai@163.com>
2018-06-25 11:51:32 +0800

    Update iSender.go

--- iSender.go
+++ iSender.go
@@ -1,23 +1,64 @@
 package main
 
 import (
-	"sync/atomic"
-	"fmt"
-	"time"
-	"bytes"
-	"os"
-	"io/ioutil"
-	"encoding/json"
 	"github.com/DingHub/iSender/util"	
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 )
 
 var (
-	succeeded uint64
-	sended    uint64
 	client    = initClient()
 	headers   = initHeaders()
 )
+
+func main() {
+	fmt.Printf("Begin-- threads: %d, requests for each thread: %d\n",
+		util.Input.Threads, util.Input.Requests)
+
+	cashedChans := make(chan uint64, util.Input.Threads)
+	for i := uint64(0); i < util.Input.Threads; i++ {
+		go request4Thread(i, cashedChans)
+	}
+	finishedThreads := uint64(0)
+	for finishedThreads < util.Input.Threads {
+		finishedThreads += <-cashedChans
+	}
+
+	fmt.Println("--End")
+}
+
+func request4Thread(thread uint64, c chan uint64) {
+	for i := uint64(0); i < util.Input.Requests; i++ {
+		request := makeRequest()
+		response, err := client.SendRequest(request)
+		if err == nil {
+			if util.Input.ShowDetail {
+				resBody, _ := ioutil.ReadAll(response.Body)
+				fmt.Printf("[thread %d, request %d] response CODE: %d; response BODY: %s\n",
+					thread, i, response.StatusCode, resBody)
+			} else {
+				fmt.Println("response CODE:", response.StatusCode)
+			}
+			response.Body.Close()
+		} else {
+			if util.Input.ShowDetail {
+				fmt.Printf("[thread %d, request %d] error: %s\n",
+					thread, i, err.Error())
+			} else {
+				fmt.Println("error:", err)
+			}
+		}
+		if i < util.Input.Requests-1 {
+			time.Sleep(time.Duration(util.Input.Delay) * time.Millisecond)
+		}
+	}
+	c <- 1
+}
 
 func initClient() (client *util.Client) {
 	if util.Input.Ca == "" {
@@ -50,44 +91,6 @@ func initHeaders() (headers map[string]string) {
 		}
 	}
 	return
-}
-
-func main() {
-	fmt.Println("Begin-- threads:", util.Input.Threads, ", requests for each thread:", util.Input.Requests)
-	for i := uint64(0); i < util.Input.Threads; i++ {
-		go request4Thread(i)
-	}
-	for atomic.LoadUint64(&sended) < util.Input.Threads*util.Input.Requests {
-		time.Sleep(time.Second)
-	}
-	fmt.Println("End-- sended requests:", atomic.LoadUint64(&sended), ", succeed:", atomic.LoadUint64(&succeeded))
-}
-
-func request4Thread(thread uint64) {
-	for i := uint64(0); i < util.Input.Requests; i++ {
-		request := makeRequest()
-		response, err := client.SendRequest(request)
-		if err == nil {
-			if util.Input.ShowDetail {
-				resBody, _ := ioutil.ReadAll(response.Body)
-				fmt.Printf("[thread %d, request %d] response CODE: %d; respnese BODY: %s\n",
-					thread, i, response.StatusCode, resBody)
-			} else {
-				fmt.Println("response CODE:", response.StatusCode)
-			}
-			response.Body.Close()
-			atomic.AddUint64(&succeeded, 1)
-		} else {
-			if util.Input.ShowDetail {
-				fmt.Printf("[thread %d, request %d] error: %s\n",
-					thread, i, err.Error())
-			} else {
-				fmt.Println("error:", err)
-			}
-		}
-		atomic.AddUint64(&sended, 1)
-		time.Sleep(time.Duration(util.Input.Delay) * time.Millisecond)
-	}
 }
 
 // request can't be reused, because after sended, it's body will be clened by http package~
